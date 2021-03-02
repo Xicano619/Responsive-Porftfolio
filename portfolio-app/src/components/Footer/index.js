dcea7356c51f002f26c6f36b3e4b464d76821ffa
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-02 13:31:30 -0800

    Footer component with email and links to github and LinkedIn accounts.

---
+++ portfolio-app/src/components/Footer/index.js
@@ -0,0 +1,35 @@
+import React from 'react';  
+
+const Footer = () => {
+
+    return (
+
+        <footer className="text-center" style="background-color:dimGrey;">
+            <div class="icon-container">
+              
+              <p className=" fixed-bottom ">Ivan.Sillasnavarro@gmail.com
+                   
+    
+                <a className="align-center" href="https://www.linkedin.com/in/ivan-sillas-navarro-018662182/"
+                     target="_blank"><img src="./assets/images/icons8-linkedin-48.png" alt=""><span class="media-text"></span>"</img>
+                </a>
+                    
+                <a href="https://github.com/Xicano619"
+                    target="_blank"><img src="./assets/images/icons8-github-48.png" alt=""></img>
+                </a>
+              </p> 
+            </div>
+
+
+
+        </footer>
+
+
+
+    );
+
+
+};
+
+
+export default Footer;
