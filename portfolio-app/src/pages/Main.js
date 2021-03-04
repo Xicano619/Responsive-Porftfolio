e24977a13f5397fd20b5bb647a5962eb10ca8dd0
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-04 00:22:47 -0800

    Created the Main page .

--- portfolio-app/src/pages/Main.js
+++ portfolio-app/src/pages/Main.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
-   const Main = () => {
+import { Container } from '../components/Grid/index';
+import BioContainer from '../components/BioContainer/index';
+import NavBar from '../components/NavBar/index';
+import Footer from '../components/Footer/index';
+   
+const Main = () => {
         return (
-
-
-
+            <Container>
+                <NavBar/> 
+                <br/> 
+                <BioContainer/>
+                <br/>
+                <Footer/>
+            </Container>
         );
 
 
