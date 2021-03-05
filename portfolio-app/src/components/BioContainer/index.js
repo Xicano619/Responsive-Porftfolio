27da96950b5626ecb410a402e285bb4242bdec06
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-05 15:55:44 -0800

    Fixed the <img> into jsx react by importing file and refering it in src={<name of import>}.

--- portfolio-app/src/components/BioContainer/index.js
+++ portfolio-app/src/components/BioContainer/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Row, Col,  Container} from '../Grid/index';
+import aboutPic from '../assets/images/Profile-Pic.JPG';
 
     const BioContainer = () => {
 
         return (
             <Container> 
                 <Row>
-                     <Col sm-12>
+                     <Col size='sm-12'>
                          <Row>   
-                            <Col sm-8>
+                            <Col size='sm-8'>
                                 <div className='main-header'>
                                     <h2>Bio</h2>
                                 </div>
                                 <hr/>
-                                <img id='mainPic' className='img img-thumbnail float-left' src="../../portfolio-app/" alt="Head shot of Iván "/>
+                                <img id='mainPic' className='img img-thumbnail float-left' src={aboutPic} alt="Head shot of Iván "/>
 
                                 <p>Hello world! I am Iván Sillas-Navarro. A husband, a community member, a teacher and currently a student of computer programming at UCSD-Extension. Born in México, and raised in San Diego, California. The first but not the last, in my family to attend and graduate university. A soul with a purpose.</p>                        
                                 <p> I enjoy jogs on the beach accompanied by ocean swims; an exercise enthusiast. An avid reader in attempts to learn everything there is about life. What is LIFE? And why is there LIFE?</p>
--- portfolio-app/src/components/ContactContainer/index.js
+++ portfolio-app/src/components/ContactContainer/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Col, Row, Container } from '../Grid/index';
+import NavBar from '../NavBar/index';
+import iconLinkIn from '../assets/images/icons8-linkedin-48.png';
+import iconGitHub from '../assets/images/icons8-github-48.png';
 
 const ContactContainer = () => {
 
     return (
-        <Container fluid>
-            
-            <h1 className="contact-title">Contact</h1>
-            <Row>
-                <Col sm-4>
-                    <div className="test-wrapper">
+        <Container>
+                <NavBar/>
+                <div className="container">
+                    <h1 className="main-title">Contact</h1>
+                </div>
+                <Row>
+                    <Col size='sm-4'>
+                        <div className="test-wrapper">
                         <p>Link up with me on LinkedIn and GitHub.</p>
                         <br/>
                         <br/>
@@ -17,27 +22,25 @@ const ContactContainer = () => {
                         <br/>
                         <p>“There is no better than adversity. Every defeat, every heartbreak, every loss, contains its own seed, its own lesson on how to improve your performance next time.”</p>
                         <p>— Malcolm X</p>
-                    </div>
-                </Col>
-                <Col sm-4></Col>
-                <Col sm-4>
-                    <p>
-                    <label class="main-email">Ivan.sillasnavarro@gmail.com</label>
-                    </p>
-                    <p>
-                        <a href="https://www.linkedin.com/in/ivan-sillas-navarro-018662182/"
-                             target="_blank"><img src="./assets/images/InIcon.png" alt=""/><span class="media-text"></span>
-                        </a>
-                    </p>
-                    <p>
-                        <a href="https://github.com/Xicano619"
-                            target="_blank"><img src="./assets/images/github-24.png" alt=""/><span class="media-text"></span>
-                        </a>
-                    </p>
-                </Col>
-            </Row>
-
-        </Container>
+                        </div>
+                    </Col>
+                    <br/>
+                    <Col size='sm-4'>
+                        <p>
+                        <label class="main-email">Ivan.sillasnavarro@gmail.com</label>
+                        </p>
+                        <p>
+                            <a href="https://www.linkedin.com/in/ivan-sillas-navarro-018662182/"
+                            target="_blank"><img src={iconLinkIn} alt=""/><span class="media-text"></span></a>
+                        </p>
+                        <p>
+                            <a href="https://github.com/Xicano619"
+                            target="_blank"><img src={iconGitHub} alt=""/><span class="media-text"></span></a>
+                        </p>
+                    </Col>
+                </Row>
+                
+            </Container>
 
     );
 
