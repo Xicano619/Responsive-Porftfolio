b9201a8a541f43475a5285e7553c5a61bafac04b
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-06 00:22:36 -0800

    Cleaned code condensing the dead space.

--- portfolio-app/src/pages/Contact.js
+++ portfolio-app/src/pages/Contact.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import ContactContainer from '../components/ContactContainer/index';
+    
+const Contact = () => {
+    return <ContactContainer/>;
+};
 
-
-     const Contact = () => {
-        return <ContactContainer/>;
-    };
 export default Contact;
