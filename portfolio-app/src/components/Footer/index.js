92ec45859dd0ac78a50fdc856d6c27eeb1c48fe0
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-06 00:37:07 -0800

    Restructured code to align correctly; clean code.

--- portfolio-app/src/components/Footer/index.js
+++ portfolio-app/src/components/Footer/index.js
@@ -3,35 +3,21 @@ import iconLinkIn from '../assets/images/icons8-linkedin-48.png';
 import iconGitHub from '../assets/images/icons8-github-48.png';
 
 const Footer = () => {
-
-    return (
-
-        <footer className="text-center" style={{ backgroundColor: "#696969"}}>
-            <div class="icon-container">
-              
-              <p className=" fixed-left">Ivan.Sillasnavarro@gmail.com
-                   
-    
-                <a className="align-center" href="https://www.linkedin.com/in/ivan-sillas-navarro-018662182/"
-                     target="_blank"><img src={iconLinkIn} alt=""/><span class="media-text"></span>
-                </a>
+  return (
+    <footer className="text-center" style={{ backgroundColor: "#696969"}}>
+      <div class="icon-container">
+        <p className=" fixed-left">Ivan.Sillasnavarro@gmail.com
+          <a className="align-center" href="https://www.linkedin.com/in/ivan-sillas-navarro-018662182/"
+            target="_blank"><img src={iconLinkIn} alt=""/><span class="media-text"></span>
+          </a>
                     
-                <a href="https://github.com/Xicano619"
-                    target="_blank"><img src={iconGitHub} alt=""/>
-                </a>
-              </p> 
-            </div>
-
-
-
-        </footer>
-
-
-
-    );
-
-
+          <a href="https://github.com/Xicano619"
+            target="_blank"><img src={iconGitHub} alt=""/>
+          </a>
+        </p> 
+      </div>
+    </footer>
+  );
 };
-
 
 export default Footer;
