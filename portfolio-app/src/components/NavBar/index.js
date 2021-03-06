3b3d98b8015ac19fbebc926cb2ea8516f45b9530
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-06 00:41:18 -0800

    Href linked to PortfolioPage .

--- portfolio-app/src/components/NavBar/index.js
+++ portfolio-app/src/components/NavBar/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import BioPage from '../../pages/Main';
 import ContactPage from '../../pages/Contact';
+import PortfolioPage from '../../pages/Portfolio';
 
 
-     const NavBar = () => {
-        return (
-            <nav className="navbar navbar-dark bg-dark">
-                <a className="navbar-brand" >Iván Sillas-Navarro</a>
-               
-                <ul className="nav justify-content-end">
-                    <li className="nav-item">
-                            <a className="nav-link active" href={BioPage}>About <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="portfolio.html">Portfolio</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href={ContactPage}>Contact</a>
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" >Iván Sillas-Navarro</a>
+            <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    <a className="nav-link active" href={BioPage}>About <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href={PortfolioPage}>Portfolio</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href={ContactPage}>Contact</a>
 
-                    </li>
-                </ul>
+                </li>
+            </ul>
                 
-            </nav>
-        );
-    };
+        </nav>
+    );
+};
     
 export default NavBar;
