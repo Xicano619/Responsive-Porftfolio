dace8a810f83562de8a5c5b6d98978f8218b780e
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-06 00:20:27 -0800

    Removed button and shifted links to the right.

--- portfolio-app/src/components/NavBar/index.js
+++ portfolio-app/src/components/NavBar/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import BioPage from '../../pages/Main';
+import ContactPage from '../../pages/Contact';
+
 
      const NavBar = () => {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Iván Sillas-Navarro</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-
-                <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a class="nav-link active" href="index.html">About <span class="sr-only">(current)</span></a>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" >Iván Sillas-Navarro</a>
+               
+                <ul className="nav justify-content-end">
+                    <li className="nav-item">
+                            <a className="nav-link active" href={BioPage}>About <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a class="nav-link" href="portfolio.html">Portfolio</a>
+                            <a className="nav-link" href="portfolio.html">Portfolio</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="contact.html">Contact</a>
+                        <a className="nav-link" href={ContactPage}>Contact</a>
 
-                        </li>
-                    </ul>
-                </div>
+                    </li>
+                </ul>
+                
             </nav>
         );
     };
