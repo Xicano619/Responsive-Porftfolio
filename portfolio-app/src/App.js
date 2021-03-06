3fd68cc40e6f1f046dfeeee51feaf192522d04cd
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-03-06 00:18:05 -0800

    Added Portfolio/ route.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Main from './pages/Main';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Main} />
       <Route exact path="/:Contact" component={Contact} />
+      <Route exact path="/:Portfolio" component={Portfolio} />
     </Router>
     
   );
